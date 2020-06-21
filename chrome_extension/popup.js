a90d9a81897a0b32ef28b7bb40fc1ebe6585f567
Daniel Olshansky <olshansky.daniel@gmail.com>
2020-06-21 09:48:31 -0700

    Added chrome extension hot reloading script
    https://medium.com/@xpl/hot-reloading-for-chrome-extensions-3da296916286

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -8,7 +8,6 @@ chrome.storage.sync.get('color', function(data) {
 changeColor.onclick = function(element) {
    let color = element.target.value;
    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-     // console.log("TEST");
      chrome.tabs.executeScript(
          tabs[0].id,
          {code: 'document.body.style.backgroundColor = "' + color + '";'});
