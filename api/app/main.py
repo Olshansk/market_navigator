6970932af719dbec96d4bffdb3f80ca72272dda2
Daniel Olshansky <olshansky.daniel@gmail.com>
2020-06-21 20:29:45 -0700

    FastAPI serving on port 8080 from docker container

--- api/app/main.py
+++ api/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -19,3 +20,6 @@ async def read_item(item_id: int, q: str = None):
 @app.post("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
